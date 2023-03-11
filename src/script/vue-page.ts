d0028ae3faec24d80b97177f020f1dce42f1a431
ryo <bocchi.se@gmail.com>
2023-03-11 20:34:08 +0900

    ルート変更

--- src/script/vue-page.ts
+++ src/script/vue-page.ts
@@ -1,27 +1,27 @@
 export const VuePage = {
   home: {
-    path: '/home',
+    path: '/vue-modal-sample/home',
     name: 'home',
     meta: {
       title: 'home',
     },
   },
   modal1: {
-    path: '/modal-1',
+    path: '/vue-modal-sample/modal-1',
     name: 'modal1',
     meta: {
       title: '普通のモーダル',
     },
   },
   modal2: {
-    path: '/modal-2',
+    path: '/vue-modal-sample/modal-2',
     name: 'modal2',
     meta: {
       title: 'フォーカス制御されたモーダル1',
     },
   },
   modal3: {
-    path: '/modal-3',
+    path: '/vue-modal-sample/modal-3',
     name: 'modal3',
     meta: {
       title: 'フォーカス制御されたモーダル2',
